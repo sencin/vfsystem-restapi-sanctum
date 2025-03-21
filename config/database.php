cf7e95f4a12f4814437baff650c4598b181ec6f7
sencin <zeraming071@gmail.com>
2025-03-21 13:27:00 +0800

    added d1 support laravel

--- config/database.php
+++ config/database.php
@@ -30,6 +30,16 @@ return [
     */
 
     'connections' => [
+        'd1' => [
+            'driver' => 'd1',
+            'prefix' => '',
+            'database' => env('CLOUDFLARE_D1_DATABASE_ID', ''),
+            'api' => 'https://api.cloudflare.com/client/v4',
+            'auth' => [
+                'token' => env('CLOUDFLARE_TOKEN', ''),
+                'account_id' => env('CLOUDFLARE_ACCOUNT_ID', ''),
+            ],
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
