98dbb7be7a7335a9dc36f3b8dfb06ea481aaa60b
sencin <zeraming071@gmail.com>
2025-04-12 20:32:37 +0800

    more support vue

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://uepverticalfarming.pages.dev', 'https://uepverticalfarming.vercel.app'],
+    'allowed_origins' => ['https://uepverticalfarming.pages.dev', 'https://uepverticalfarming.vercel.app', 'http://localhost:5173','http://localhost:4173'],
 
     'allowed_origins_patterns' => [],
 
