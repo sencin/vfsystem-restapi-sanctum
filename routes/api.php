257c33b28cdc95ca5a52e9cda4791dc6b1319747
sencin <zeraming071@gmail.com>
2025-06-19 17:34:17 +0800

    added more api endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\Api\PlantInformationController;
 use App\Http\Controllers\Api\PlantRequirementController;
 use App\Http\Controllers\Api\ReadingController;
+use App\Http\Controllers\Api\PlantStageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SensorController;
@@ -9,7 +10,14 @@ use App\Http\Controllers\Api\TowerController;
 use App\Http\Controllers\Web\PlantTransplantController;
 use App\Http\Controllers\Web\UserAuth;
 use App\Http\Controllers\Web\UserController;
+use App\Http\Controllers\FileController;
+
 Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/esp32websocketconfig',function(){
+        return response()->json([
+            'socketUrl'=> env('ESP32_CAMERA_WS')
+        ]);
+    });
 
     Route::apiResources([
         'plantinformations' => PlantInformationController::class,
@@ -20,18 +28,20 @@ Route::middleware('auth:sanctum')->group(function(){
         'planttransplants'  => PlantTransplantController::class
     ]);
 
+    Route::apiResource('plantinformations.plantrequirements', PlantRequirementController::class);
+    Route::apiResource('planttransplants.plantstages', PlantStageController::class);
+
+    Route::post('planttransplants/{planttransplant}/plantstages/{plantstage}/updateStatus',
+    [PlantStageController::class, 'updateStage']
+    )->name('plantstages.updatestatus');
+
     Route::post('/users/{user}/approve', [UserController::class, 'approve']);
     Route::post('/users/{user}/reject', [UserController::class, 'reject']);
     Route::get('/pendingusers', [UserController::class,'getPendingUsersAPI']);
 
-    // Route::apiResource('plantinformations', PlantInformationController::class);
-    // Route::apiResource('plantrequirements', PlantRequirementController::class);
-    // Route::apiResource('sensors', SensorController::class);
-    // Route::apiResource('towers', TowerController::class);
-    // Route::apiResource('readings', ReadingController::class);
-    // Route::apiResource('planttransplants', PlantTransplantController::class);
-
     Route::get('/getTowerId',[ReadingController::class, 'getTowerId']);
+    Route::get('/plantinformations/availableplants', [PlantInformationController::class, 'getAvailablePlants'])->name('getAvailablePlants');
+    Route::get('storage/image/{filename}', [FileController::class, 'show']);
 });
 
 Route::get('/availabletowers', [TowerController::class, 'showAllTowers']);
